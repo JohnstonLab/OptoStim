8e2877abf2fb2fc91e3bc6b323c84fe402ca4a68
Scott Dunkley <scottdunkley@gmail.com>
2018-11-01 22:17:11 +0000

    Check for correct setup.

--- run.bat
+++ run.bat
@@ -1,5 +1,11 @@
-call python\Scripts\activate.bat
+@echo off
 
-python optostim\main.py
+IF NOT EXIST python\Scripts\activate.bat (
+	COLOR C
+	echo Please run the setup batch file located in the same directory first. The virtual environment python is not setup.
+) ELSE (
+	call python\Scripts\activate.bat
+	python optostim\main.py
+)
 
 pause
